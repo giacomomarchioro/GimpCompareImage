1f225e8e9655572d7c9637588c7fe44ecb5072a0
Giacomo Marchioro <marchiorogiacomo@gmail.com>
2022-07-27 12:21:03 +0200

    Width and height are taken from the second layer

--- CompareImageAdva.py
+++ CompareImageAdva.py
@@ -3,18 +3,23 @@ from gimpfu import *
 
 def CompareImageAdva(img,drw,nc,nr,s,par):
     layers = img.layers 
-    img.resize((layers[0].width+s)*nc-s,(layers[0].height+s)*nr-s,0,0)
     count=0
+    # we copy the width and height of the second layer.
+    width = layers[1].width
+    height = layers[1].height
+    img.resize((width+s)*nc-s,(height+s)*nr-s,0,0)
     if par==True:
         count=1
     for j in range(0,nr):
         for i in range(0,nc):
-            layers[count].translate((layers[0].width+s)*i,(layers[0].height+s)*j)
+            layers[count].translate((width+s)*i,(height+s)*j)
             count+=1
             if par==True:
                 layer2=layers[0].copy()
                 img.add_layer(layer2, 0)
-                layer2.translate((layers[0].width+s)*i,(layers[0].height+s)*j)
+                layer2.translate((width+s)*i,(height+s)*j)
+                #if pdb.gimp_drawable_is_text_layer(layer2):
+                #    pdb.gimp_text_layer_set_text(layer2,'aaa')
     if par==True:
         img.remove_layer(layers[0])
             
@@ -32,7 +37,7 @@ register(
                 (PF_INT, "nc", "Number of coulum:", 0),
                 (PF_INT, "nr", "Number of row:", 0),
                 (PF_INT, "s", "Space between images:", 0),
-                 (PF_BOOL,"par","Duplicate first layer over the others:",False)
+                (PF_BOOL,"par","Duplicate first layer over the others:",False)
                 
         ],
         [],
